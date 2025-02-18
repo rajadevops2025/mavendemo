f0fdaa7d57d314dba82498ea475df7e22efa033f
rajadevops2025 <rajadevops2025@gmail.com>
2025-02-18 10:25:39 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,10 @@
+package com.example;
+
+/**
+ * Simple Java Application
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven!");
+    }
+}
